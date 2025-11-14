6508ab77a51962e33b4ec6c10b941a9d491e389a
Miguel <Miguel@DESKTOP-3TS8GNI>
2025-11-14 10:14:56 +0100

    lol sorry

--- Scripts/map.gd
+++ Scripts/map.gd
@@ -28,7 +28,7 @@ func _ready():
 		Globals.timer.start()
 	else:
 		if multiplayer.is_server():
-			if not OS.has_feature("dedicated_server") or not "s" in OS.get_cmdline_user_args() or not "server" in OS.get_cmdline_user_args():
+			if not OS.has_feature("dedicated_server"):
 				Globals.player_join(1)	
 
 			for i in multiplayer.get_peers():
